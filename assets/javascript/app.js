054e7141c0e766af46ffdc70f429575958bf0752
Dave <david.j.anderson94@gmail.com>
2018-09-27 21:59:59 -0700

    stockio now live updates the most recent stock price of the held stocks

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,11 +4,13 @@ $(document).ready(function() {
     var user = {
 
         buyingPower: 10000,
-        stocks: ["AAPL"]
+        stocks: ["AAPL", "AMZN", "GOOGL"],
+        shares: [4, 2, 8],
+        portfolio: 10000,
 
     }
 
-    var intervalId = setInterval(pullStockData, 5000);
+    var intervalId = setInterval(pullStockData, 60000);
 
 
     function pullStockData() {
@@ -17,18 +19,16 @@ $(document).ready(function() {
             var ticker = user.stocks[i];
             //console.log(ticker);
             var queryUrl = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + ticker + "&interval=5min&outputsize=full&apikey=" + alphaVantageKey;
-
+            var price = 0;
+            console.log(i);
             $.ajax({
                 url: queryUrl,
                 method: 'GET'
 
             }).then(function (response){
+                console.log(response);
 
-                //console.log(response);
-                //console.log(response[0]);
-                //console.log(response);
                 var prices = response["Time Series (5min)"];
-                //console.log(prices);
 
                 var min = 100000000000000;
                 var mostRecentTime = "";
@@ -44,17 +44,31 @@ $(document).ready(function() {
                 console.log(mostRecentTime);
 
                 console.log(prices[mostRecentTime]);
-                var price = prices[mostRecentTime]["4. close"];
-                console.log(price);
+                price = prices[mostRecentTime]["4. close"];
+
+                //console.log(user.portfolio);
+                for (var j = 0; j < user.stocks.length; j++) {
+                    user.portfolio = user.portfolio + price * user.shares[j];
+                    console.log(user.portfolio)
+                    $("#"+ user.stocks[j] + "Price").text("$"+price);
+                    
+                }
+               
 
                 
-
             });
 
+            
+            
+
+
         }
+        console.log(user.portfolio);
+        
 
 
     }
+    
 
 
 
