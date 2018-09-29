398767c5b5da1b6109bce4879cd8f4865ea34f71
Shanks <shanks.trae@yahoo.com>
2018-09-29 10:43:58 -0700

    TS WSJ api

--- assets/javascript/WSJapi.js
+++ assets/javascript/WSJapi.js
@@ -17,12 +17,16 @@ $(document).ready(function () {
         .then(function(response) { // After the data from the AJAX request comes back
             console.log(response.articles);
             var newsHeadLink = $("<a class=linkText>");
-            var smallDescription = $("<small>");
+            var smallDescription = $("<small>"); 
             smallDescription.text(response.articles[0].description)
             newsHeadLink.text(response.articles[0].title);
             newsHeadLink.attr("href", response.articles[0].url);
-            newsHeadLink.after(smallDescription);
             $("#news").append(newsHeadLink);
+            $("#news").append("<br></br>");
+            $("#news").append(smallDescription);
+            $("#news").append("<br></br>");
+        
+
             // grab the title and put it into a h3 element, then add a href attr to that h3 element with the url
 
 
