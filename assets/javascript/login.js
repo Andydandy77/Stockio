8cef527e2214d23e5a01a83ef3f06a1ae8bf63b4
Shanks <shanks.trae@yahoo.com>
2018-10-01 12:35:23 -0700

    TS working on user accounts with firebase

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -1,4 +1,23 @@
-console.log("linked");
+$(document).ready(function () {
 
-var userName = $(".loginButton");
-var Password = $(".signUpButton");
+    console.log("linked mannn");
+
+    var userName = $(".userName").val();
+    var password = $(".passWord").val();
+
+    $(".signUpButton").on("click", function() {
+        userName = $(".userName").val();
+        password = $(".passWord").val();
+        console.log(userName);
+        console.log(password);
+    });
+
+    $(".loginButton").on("click", function() {
+        userName = $(".userName").val();
+        password = $(".passWord").val();
+        console.log(userName);
+        console.log(password);
+    });
+
+    // now that I have the username and password I want th
+});
