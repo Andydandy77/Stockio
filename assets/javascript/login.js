28d8b2e5713f7e9a272f0bca9061840bfaa7787f
Shanks <shanks.trae@yahoo.com>
2018-10-02 21:16:22 -0700

    login page complete

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -1,23 +1,68 @@
 $(document).ready(function () {
-
+    
     console.log("linked mannn");
+});
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyC0tz_bNSxnWAANS4j6XKDzHIDz9WFGI70",
+        authDomain: "fir-practice-7e0ed.firebaseapp.com",
+        databaseURL: "https://fir-practice-7e0ed.firebaseio.com",
+        projectId: "fir-practice-7e0ed",
+        storageBucket: "fir-practice-7e0ed.appspot.com",
+        messagingSenderId: "10233592685"
+    };
+    firebase.initializeApp(config);
 
+
+function login() {  
     var userName = $(".userName").val();
     var password = $(".passWord").val();
+    firebase.auth().signInWithEmailAndPassword(userName, password).catch(function(error) {
+        console.log(error);
+        console.log(userName + password)
 
-    $(".signUpButton").on("click", function() {
-        userName = $(".userName").val();
-        password = $(".passWord").val();
-        console.log(userName);
-        console.log(password);
+        // Handle Errors here.
+
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        window.alert("Error: " + errorMessage);
+        
+        });
+        
+};
+
+function signUp() {
+    var userName = $(".userName").val();
+    var password = $(".passWord").val();
+    firebase.auth().createUserWithEmailAndPassword(userName, password).catch(function(error) {
+        console.log("Signed Up")
+        // Handle Errors here.
+
+        // var errorCode = error.code;
+        // var errorMessage = error.message;
+        // window.alert("Error: " + errorMessage);
+        
+        
+    
     });
+};
 
-    $(".loginButton").on("click", function() {
-        userName = $(".userName").val();
-        password = $(".passWord").val();
-        console.log(userName);
-        console.log(password);
-    });
 
-    // now that I have the username and password I want th
+function logout() {
+    firebase.auth().signOut()
+        // Sign-out successful.
+        console.log("They signed out Trae");
+    
+    
+
+};
+
+firebase.auth().onAuthStateChanged(function (user) {
+    console.log("statechanged");
+    // logout();
+    if(user) {
+        console.log("fired again");
+        // window.location = '../../portfolio.html'; //After successful login, user will be redirected to portfolio.html
+      }
+
 });
